ad3561844a7edf96f8d22a176a90f85c73527f51
alexv95 <alexmb1995@gmail.com>
2023-04-12 11:15:18 -0400

    :sparkles: working ml model ex

--- app/core/service/flight_service.py
+++ app/core/service/flight_service.py
@@ -1,11 +1,11 @@
 
 
 from  app.core.dto.flight_prediction_dto import FlightPredictionDto
-
+from app.core.ml.flight_prediction import mlPredictionFlight
 
 class FlightService:
     def prediction(self,input_value):
-        predictedValueDto = FlightPredictionDto(input_value,"abc")
+        predictedValueDto = FlightPredictionDto(input_value,mlPredictionFlight.predict_pipeline(input_value))
         return predictedValueDto.__dict__
  
 flightService:object = FlightService()
