2a6e4c70d4123155b4f60b6ff0d4d0853756fd4d
Jirachi13@ <alexmb1995@gmail.com>
2023-04-13 16:59:23 -0400

    :bug: add exception for empty values

---
+++ app/core/exception/invalid_input.py
@@ -0,0 +1,9 @@
+
+
+class InvalidInputException:
+    status_code = 400
+    message = "Invalid input, value input_value is  empty"
+
+
+
+invalid_input_exception = InvalidInputException()
--- app/core/service/flight_service.py
+++ app/core/service/flight_service.py
@@ -1,12 +1,20 @@
 
 
+from fastapi import HTTPException
 from  app.core.dto.flight_prediction_dto import FlightPredictionDto
 from app.core.ml.flight_prediction import mlPredictionFlight
 
+
+
 class FlightService:
     def prediction(self,input_value):
-        predictedValueDto = FlightPredictionDto(input_value,mlPredictionFlight.predict_pipeline(input_value))
-        return predictedValueDto.__dict__
+        if(not(input_value and input_value.strip())):
+            
+        
+            raise HTTPException(status_code=400,detail="Input_value is required")
+        else:
+            predictedValueDto = FlightPredictionDto(input_value,mlPredictionFlight.predict_pipeline(input_value))
+            return predictedValueDto.__dict__
  
 flightService:object = FlightService()
 
